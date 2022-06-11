3530f874dbf090c25c1392da48dcb75d28d321ad
Mrdynamic-soni <ssoni5930@gmail.com>
2022-06-11 20:57:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import IndiaMap from './IndiaMap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import 'react-bootstrap';
+import Imsvg from './Icons/Imsvg';
+import Upsvg from './Icons/Upsvg'
+import {Route, Routes } from 'react-router-dom';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+     <Routes>
+        <Route exact path="/" element={<Imsvg/>}/>
+        <Route exact path="/StateRoute" element={<Upsvg/>}/>
+    </Routes>
+    </>
+  )
 }
 
 export default App;
